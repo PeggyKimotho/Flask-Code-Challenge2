ff437e55a2c125976ab001c60f4283021dfab0ef
Peggy <peggykimotho@gmail.com>
2023-10-05 05:07:11 +0000

    completed

--- seed.py
+++ seed.py
@@ -12,7 +12,14 @@ powers_data = [
 heroes_data = [
     {"name": "Kamala Khan", "super_name": "Ms. Marvel"},
     {"name": "Doreen Green", "super_name": "Squirrel Girl"},
-    # Add more heroes here
+    {"name": "Gwen Stacy", "super_name": "Spider-Gwen"},
+    {"name": "Janet Van Dyne", "super_name": "The Wasp"},
+    {"name": "Wanda Maximoff", "super_name": "Scarlet Witch"},
+    {"name": "Carol Danvers", "super_name": "Captain Marvel"},
+    {"name": "Jean Grey", "super_name": "Dark Phoenix"},
+    {"name": "Ororo Munroe", "super_name": "Storm"},
+    {"name": "Kitty Pryde", "super_name": "Shadowcat"},
+    {"name": "Elektra Natchios", "super_name": "Elektra"}
 ]
 
 strengths = ["Strong", "Weak", "Average"]
